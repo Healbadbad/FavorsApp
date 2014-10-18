ce00ba397140620bebdbe68c650eaca1711227d2
Scub3d <scubedtechnologiesllc@gmail.com>
2014-10-18 12:02:20 -0400

    merged aint shit but.... HOES N JESUS

--- FavorsApp/src/hairforce/main/favorsapp/MainActivity.java
+++ FavorsApp/src/hairforce/main/favorsapp/MainActivity.java
@@ -55,14 +55,17 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
 
 	public void onSectionAttached(int number) {
 		switch (number) {
+		
 		case 1:
 			mTitle = getString(R.string.title_section1);
-			break;
 		case 2:
 			mTitle = getString(R.string.title_section2);
 			break;
 		case 3:
 			mTitle = getString(R.string.title_section3);
+			break;
+		case 4:
+			mTitle = getString(R.string.title_section4);
 			break;
 		}
 	}
